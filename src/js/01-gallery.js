6c32647a9bec57bf136745a4f2de49764cc16be0
DarkSwarp <mihkleban@gmail.com>
2023-02-23 13:30:32 +0200

    added task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+// Створення розмітки галереї
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class = "gallery__item" href = "${original}" target = "_self"><img class = "gallery__image" src = "${preview}" alt = "${description}"></a>`
+  )
+  .join('');
+galleryBox.insertAdjacentHTML('beforeend', galleryMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
